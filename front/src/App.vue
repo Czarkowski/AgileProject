278f7df96718755739c10c8cc3c0ce7dab8200ac
Oliwia Paliwoda <97446500+oliwia-paliwoda@users.noreply.github.com>
2025-03-20 13:48:13 +0100

    Routing init (#3)
    * test
    
    * router init
    
    ---------
    
    Co-authored-by: Oliwia Paliwoda <oliwia@LAPTOP-SGUVKNRU>

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,43 +1,33 @@
 <template>
-  <div class="flex items-center justify-center h-screen bg-gray-100">
-    <div class="bg-white p-8 rounded-lg shadow-lg w-96">
-      <h2 class="text-2xl font-bold text-center mb-6">Logowanie</h2>
-      <form @submit.prevent="handleLogin">
-        <div class="mb-4">
-          <label for="email" class="block text-gray-700">Email</label>
-          <input v-model="email" type="email" id="email" class="w-full p-2 border rounded mt-1" required />
-        </div>
-        <div class="mb-4">
-          <label for="password" class="block text-gray-700">Hasło</label>
-          <input v-model="password" type="password" id="password" class="w-full p-2 border rounded mt-1" required />
-        </div>
-        <button type="submit" class="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition">
-          Zaloguj się
-        </button>
-      </form>
-    </div>
+  <div id="app">
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      email: '',
-      password: ''
-    };
-  },
-  methods: {
-    handleLogin() {
-      console.log("Email:", this.email, "Hasło:", this.password);
-      // Tutaj można dodać obsługę logowania np. wysyłanie żądania do backendu
-    }
-  }
+  name: 'App',
 };
 </script>
 
 <style>
+
+{
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f7f7f7;
+}
+
 body {
-  font-family: Arial, sans-serif;
+  font-family: 'Arial', sans-serif;
+  height: 100%;
+  color: #181818;
 }
 </style>
