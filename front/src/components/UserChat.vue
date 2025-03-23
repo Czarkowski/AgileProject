d306f534c819995735eeee7496d3c1d63b0da7e5
Oliwia Paliwoda <97446500+oliwia-paliwoda@users.noreply.github.com>
2025-03-23 07:27:59 +0100

    routing placeholders (#6)

---
+++ front/src/components/UserChat.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  NAME: "UserChat"
+}
+</script>
+
+<template>
+  <div class="test">{{ $route.path }}</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ front/src/components/UserSearch.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  NAME: "UserSearch"
+}
+</script>
+
+<template>
+  <div class="test">{{ $route.path }}</div>
+</template>
+
+<style scoped>
+
+</style>
