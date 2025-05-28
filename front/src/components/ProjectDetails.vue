fdd35fddf2d002faa5038aae6ab336648b2862a3
Aleksandra Maciejak <aleksandra.maciejak123@gmail.com>
2025-05-28 20:38:25 +0200

    Added front for project chat

--- front/src/components/ProjectDetails.vue
+++ front/src/components/ProjectDetails.vue
@@ -1,12 +1,21 @@
-<script>
-export default {
-  NAME: "ProjectDetails"
-}
+<script setup lang="ts">
+import Chat from '@/components/ProjectChat.vue'
 </script>
 
 <template>
-  <div class="test">{{ $route.path }}</div>
+  <div class="project-details">
+    <h2 class="title">Szczegóły projektu</h2>
+    <Chat />
+  </div>
 </template>
-<style scoped>
 
+<style scoped>
+.project-details {
+  padding: 2rem;
+}
+
+.title {
+  font-size: 1.5rem;
+  margin-bottom: 1rem;
+}
 </style>
