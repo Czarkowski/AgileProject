01ed75b11b7860ebf0f901c10c4c42c7a6add572
Oliwia Paliwoda <oliwia@LAPTOP-SGUVKNRU.>
2025-06-24 23:17:06 +0200

    members management

--- front/src/components/ProjectDetails.vue
+++ front/src/components/ProjectDetails.vue
@@ -2,6 +2,7 @@
 import Chat from '@/components/ProjectChat.vue'
 import { ProjectControllerApi } from '@/api/apis/ProjectControllerApi';
 import { Configuration } from '@/api/runtime';
+import { UserControllerApi } from '@/api/apis/UserControllerApi'
 import * as userUtils from "../user.js";
 
 export default {
@@ -11,6 +12,7 @@ export default {
 
   data() {
     return {
+      hoveredMemberId: null,
       projectId: null,
       project: undefined,
       projects: [],
@@ -22,6 +24,9 @@ export default {
       editedDescription: '',
       titleChanged: false,
       descriptionChanged: false,
+      users: [],
+      userList: [],
+      toggleUserList: false,
     };
   },
 
@@ -30,7 +35,102 @@ export default {
       this.user = userUtils.getLoggedUser();
       this.projectId = Number(this.$route.params.projectId);
       this.getAllProjects();
-      console.error(this.projects);
+      this.getAllUsers();
+    },
+
+    removeMember(memberId) {
+      this.removeUser(Number(this.projectId), Number(memberId));
+      this.members = this.members.filter(member => member.id !== memberId);
+      this.updateSelectableUsers();
+    },
+
+    updateSelectableUsers() {
+      const memberIds = this.members.map(member => member.id);
+      const ownerId = this.user.loggedUser.id;
+      this.userList = this.users.filter(user =>
+          !memberIds.includes(user.id) && user.id !== ownerId
+      );
+    },
+
+    handleSelectChange() {
+      const selectedUser = this.users.find(user => user.id === this.selectedUserId);
+      if (selectedUser && !this.members.some(member => member.id === selectedUser.id)) {
+        this.members.push({ id: selectedUser.id, username: selectedUser.username });
+        console.error(this.members);
+      }
+      this.addUser(Number(this.projectId),Number(this.selectedUserId));
+      this.selectedUserId = '';
+      this.toggleUserList = false;
+      this.updateSelectableUsers();
+    },
+
+    async getAllUsers() {
+      const configuration = new Configuration({ accessToken: this.user.token });
+      const userControllerApi = new UserControllerApi(configuration);
+
+      try {
+        const users = await userControllerApi.getAllUsers();
+        this.users = users;
+        console.error("Pobrani użytkownicy:", this.users);
+      } catch (error) {
+        console.error("Błąd podczas pobierania użytkowników:", error);
+        this.errorMessage = 'Nie udało się pobrać użytkowników.';
+      }
+    },
+
+    showUserList(){
+      this.updateSelectableUsers();
+      this.toggleUserList = true;
+      this.$nextTick(() => {
+        this.$refs.userSelect.focus();
+        this.$refs.userSelect.click();
+      });
+    },
+
+    async markAsComplete() {
+      const configuration = new Configuration({ accessToken: this.user.token });
+      const projectControllerApi = new ProjectControllerApi(configuration);
+
+      try {
+        const requestParams = { projectId: this.project.id };
+        const updatedProject = await projectControllerApi.completeProject(requestParams);
+
+        this.project = updatedProject;
+        console.error("mark complete response", this.project);
+        this.checkCompletionStatus();
+      } catch (error) {
+        console.error("Błąd podczas oznaczania projektu jako zakończony:", error);
+      }
+    },
+
+    async markAsUncomplete() {
+      const configuration = new Configuration({ accessToken: this.user.token });
+      const projectControllerApi = new ProjectControllerApi(configuration);
+
+      try {
+        const requestParams = { projectId: this.project.id };
+        const updatedProject = await projectControllerApi.uncompleteProject(requestParams);
+
+        this.project = updatedProject;
+        this.checkCompletionStatus();
+      } catch (error) {
+        console.error("Błąd podczas cofania zakończenia projektu:", error);
+      }
+    },
+
+    handleCompletion(){
+      if(this.turnedIn === 1){
+        this.markAsComplete();
+      }
+      else if (this.turnedIn === 0){
+        this.markAsUncomplete();
+      }
+    },
+
+    checkCompletionStatus(){
+      if(this.project.completionDate === undefined)
+        this.turnedIn = 0;
+      else this.turnedIn = 1;
     },
 
     async getAllProjects() {
@@ -46,7 +146,11 @@ export default {
           this.project = found;
           this.editedTitle = found.title;
           this.editedDescription = found.description;
+          this.members = found.members;
+
         }
+        console.error(this.project);
+        this.checkCompletionStatus();
       } catch (error) {
         console.error("Błąd podczas pobierania projektów:", error);
       }
@@ -83,6 +187,62 @@ export default {
           alert('Błąd podczas aktualizacji projektu');
         }      }
     },
+
+    async addUser(projectId, userId) {
+      console.log('addUserToProject called with:', { projectId, userId });
+
+      const configuration = new Configuration({ accessToken: this.user.token });
+      const projectControllerApi = new ProjectControllerApi(configuration);
+
+      try {
+        const requestBody = { projectId, userId };
+
+        await projectControllerApi.addUserToProject({ userAndProjectRequestBody: requestBody });
+
+        console.log('Użytkownik dodany pomyślnie');
+        alert('Użytkownik dodany do projektu');
+      } catch (error) {
+        if (
+            error instanceof SyntaxError &&
+            error.message.includes('Unexpected end of JSON input')
+        ) {
+          console.log('Użytkownik dodany pomyślnie (pusta odpowiedź)');
+          alert('Użytkownik dodany do projektu');
+        } else {
+          console.error('Błąd podczas dodawania użytkownika:', error);
+          alert('Błąd podczas dodawania użytkownika');
+        }
+      }
+    },
+
+    async removeUser(projectId, userId) {
+      console.log('removeUserFromProject called with:', { projectId, userId });
+
+      const configuration = new Configuration({ accessToken: this.user.token });
+      const projectControllerApi = new ProjectControllerApi(configuration);
+
+      try {
+        const requestBody = { projectId, userId };
+
+        await projectControllerApi.deleteUserFromProject({ userAndProjectRequestBody: requestBody });
+
+        console.log('Użytkownik usunięty pomyślnie');
+        alert('Użytkownik usunięty z projektu');
+      } catch (error) {
+        if (
+            error instanceof SyntaxError &&
+            error.message.includes('Unexpected end of JSON input')
+        ) {
+          console.log('Użytkownik usunięty pomyślnie (pusta odpowiedź)');
+          alert('Użytkownik usunięty z projektu');
+        } else {
+          console.error('Błąd podczas usuwania użytkownika:', error);
+          alert('Błąd podczas usuwania użytkownika');
+        }
+      }
+    },
+
+
 
     startEditing(field) {
       this.editingField = field;
@@ -129,16 +289,48 @@ export default {
       <div class="row">
         <div class="label">Ukończony</div>
         <div class="value">
-          <input class="checkbox" type="checkbox" id="turnedIn" v-model="project.completionDate" :true-value="1" :false-value="0" />
+          <input class="checkbox" type="checkbox" id="turnedIn" v-model="turnedIn" :true-value="1" :false-value="0" @change="handleCompletion()" />
         </div>
+      </div>
+      <div v-if="this.turnedIn === 1" class="row">
+        <div class="label">Data ukończenia</div>
+        <div class="value">{{this.project.completionDate}}</div>
       </div>
       <div class="row">
         <div class="label">Właściciel</div>
-        <div class="value">{{ project.ownerId }}</div>
+        <div class="value">{{ user.loggedUser.username }}</div>
       </div>
       <div class="row">
         <div class="label">Członkowie</div>
-        <div class="value">{{ project.members }}</div>
+        <div class="value">
+          <ul>
+            <li
+                v-for="member in members"
+                :key="member.id"
+                class="member-item"
+                @mouseenter="hoveredMemberId = member.id"
+                @mouseleave="hoveredMemberId = null"
+            >
+              {{ member.username }}
+              <span
+                  class="remove-icon"
+                  v-if="hoveredMemberId === member.id"
+                  @click="removeMember(member.id)"
+              >
+    ×
+  </span>
+            </li>
+          </ul>
+        </div>
+        <div v-if="!toggleUserList" class="add-button" @click="showUserList()">+</div>
+
+        <select style="height: fit-content" v-if="toggleUserList" v-model="selectedUserId" @change="handleSelectChange"   ref="userSelect">
+          <option v-for="user in userList" :key="user.id" :value="user.id">
+            {{ user.username }}
+          </option>
+        </select>
+
+
       </div>
     </div>
 
@@ -173,10 +365,45 @@ export default {
 
 }
 
+.add-button{
+  background-color: #0d6efd;
+  width:3vmin;
+  border-radius: 1vmin;
+  text-align: center;
+  font-weight: bold;
+  height: fit-content;
+}
+
 .value {
   flex: 1;
   font-family: 'Arial Black', sans-serif;
 
+}
+
+ul, ol{
+  background-color: unset;
+}
+
+.member-item {
+  display: flex;
+  align-items: center;
+  padding: 0.3rem 0.5rem;
+  border-radius: 4px;
+}
+
+.remove-icon {
+  cursor: pointer;
+  color: white;
+  background-color: red;
+  width: 3vmin;
+  text-align: center;
+  border-radius: 1vmin;
+  font-weight: bold;
+  margin-left: 1vmin;
+}
+
+.member-item:hover .remove-icon {
+  display: inline;
 }
 
 .project-details {
