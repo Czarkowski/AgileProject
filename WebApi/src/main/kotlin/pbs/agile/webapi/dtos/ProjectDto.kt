88c8f3dcd5736500fd9bda68bd5d71c084b068f0
Czarkowski <cezarytytko@gmail.com>
2025-06-24 09:00:52 +0200

    addCompletionDateToProjectDTO

--- WebApi/src/main/kotlin/pbs/agile/webapi/dtos/ProjectDto.kt
+++ WebApi/src/main/kotlin/pbs/agile/webapi/dtos/ProjectDto.kt
@@ -1,10 +1,13 @@
 package pbs.agile.webapi.dtos;
 
+import java.time.LocalDateTime
+
 data class ProjectDto (
     val id: Long,
     val title: String,
     val description: String,
     val ownerId: Long,
         val members: List<UserDto>,
+        val completionDate: LocalDateTime?,
 )
 
